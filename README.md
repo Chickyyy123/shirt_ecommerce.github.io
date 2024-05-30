c5293940ca173302365dac1fa085f7835b673165
Heang Kimhak <heangkimhak11@gmail.com>
2024-05-30 13:50:51 +0700

    complete

---
+++ README.md
@@ -0,0 +1,10 @@
+List Features
+
+- Home
+- Buy T-shirt
+- Woman
+- Man
+- About
+- Contact
+
+I build with (html, css, javaScript, and bootstrap)
---
+++ assets/javaScript/script.js
@@ -0,0 +1,216 @@
+// Change Image product-001
+function changeImage1() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/1.png";
+}
+function changeImage2() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/2.png";
+}
+// Change Image product-002
+function changeImage3() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/3.png";
+}
+function changeImage4() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/4.png";
+}
+// Change Image product-003
+function changeImage5() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/5.png";
+}
+function changeImage6() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/6.png";
+}
+// Change Image product-004
+function changeImage7() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/7.png";
+}
+function changeImage8() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/8.png";
+}
+// Change Image product-005
+function changeImage9() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/9.png";
+}
+function changeImage10() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/10.png";
+}
+// Change Image product-006
+function changeImage11() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/11.png";
+}
+function changeImage12() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/12.png";
+}
+// Change Image product-007
+function changeImage13() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/13.png";
+}
+function changeImage14() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/14.png";
+}
+// Change Image product-008
+function changeImage15() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/15.png";
+}
+function changeImage16() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/16.png";
+}
+// Change Image product-009
+function changeImage17() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/17.png";
+}
+function changeImage18() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/18.png";
+}
+// Change Image product-010
+function changeImage19() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/19.png";
+}
+function changeImage20() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/20.png";
+}
+// Change Image product-011
+function changeImage21() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/21.png";
+}
+function changeImage22() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/22.png";
+}
+// Change Image product-012
+function changeImage23() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/23.png";
+}
+function changeImage24() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/24.png";
+}
+// Change Image product-013
+function changeImage25() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/25.png";
+}
+function changeImage26() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/26.png";
+}
+// Change Image product-014
+function changeImage27() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/27.png";
+}
+function changeImage28() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/28.png";
+}
+// Change Image product-015
+function changeImage29() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/29.png";
+}
+function changeImage30() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/30.png";
+}
+// Change Image product-016
+function changeImage31() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/31.png";
+}
+function changeImage32() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/32.png";
+}
+// Change Image product-017
+function changeImage33() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/33.png";
+}
+function changeImage34() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/34.png";
+}
+// Change Image product-018
+function changeImage35() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/35.png";
+}
+function changeImage36() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/36.png";
+}
+// Change Image product-019
+function changeImage37() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/37.png";
+}
+function changeImage38() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/38.png";
+}
+// Change Image product-020
+function changeImage39() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/39.png";
+}
+function changeImage40() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/40.png";
+}
+// Change Image product-021
+function changeImage41() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/41.png";
+}
+function changeImage42() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/42.png";
+}
+// Change Image product-022
+function changeImage43() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/43.png";
+}
+function changeImage44() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/44.png";
+}
+// Change Image product-023
+function changeImage45() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/45.png";
+}
+function changeImage46() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/46.png";
+}
+// Change Image product-024
+function changeImage47() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/47.png";
+}
+function changeImage48() {
+  var img = document.getElementById("image");
+  img.src = "../../assets/image/48.png";
+}
